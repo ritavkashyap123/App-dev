4a79689438f0e2141775ae34654851431e2b6e04
Ritav Kashyap <ritavkashyap123@gmail.com>
2022-03-28 17:22:40 +0530

    updated

---
+++ ETE_rocks/android/app/src/main/kotlin/com/example/ete_rocks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ete_rocks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ETE_rocks/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:
+  ));
+  
+  class  extends StatelessWidget {
+    const ({Key? key}) : super(key: key);
+  
+    @override
+    Widget build(BuildContext context) {
+      return ;
+    }
+  }
+  
+}
+
+
+
